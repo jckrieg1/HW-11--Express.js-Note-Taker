890cc031ec4832ddbe56eb256132a58e205a950f
jckrieg1 <joeyck101@gmail.com>
2022-11-08 09:25:27 -0600

    fixed PORT

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const apiroutes = require('./routes/apiroutes');
 const htmlroutes = require('./routes/htmlroutes');
 const app = express();
-const PORT = 3001 || process.env.PORT;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
